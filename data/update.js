705de1270b16bf855fa598e30bdd532f7da21053
Juan Sierra <jksierra@gmail.com>
2018-11-24 09:46:41 +0000

    Completed the 'Tech words'/'jobs content' match routine

--- data/update.js
+++ data/update.js
@@ -3,21 +3,48 @@ const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const config = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
-const keyArrays = ['go', 'js', 'lang', '我哈', '你呀'];
+const keyArrays = ['go', 'js', 'lang', 'react'];
+const KeywordFilter = require('keyword-filter');
+const filter = new KeywordFilter();
+var keyCount = [];
+for (const item of keyArrays){
+    keyCount[item] = 0;
+};
+console.log(keyCount);
+console.log("va")
+filter.init(keyArrays);
 
 for (const month of config.months) {
     console.log(month.url)
     request(month.url, function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    
-    const $ = cheerio.load(body);
-    $(".athing .ind img[width=\"0\"]").parentsUntil(".athing").each(function(i, item){
-        console.log($(this).html())
-    }); 
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        
+        const $ = cheerio.load(body);
+        var imgs = $(".athing .ind img[width=\"0\"]");
+        console.log(imgs.length)
+        imgs.each(function(i, item){
+            countKeywordInBody( $(this).closest("tr").text());
+            console.log(keyCount);
+        });
     });
 }
 
-function countKeywordInBody(body){
-    console.log(filter.getOccurances(content))
+
+
+function countKeywordInBody(content){
+    var occurances = filter.getOccurances(content);
+
+    var words = occurances.map(function (item) {
+        return item.value;
+    });
+    console.log(words)
+    var filtered = words.filter(function(item, index){
+        return words.indexOf(item) >= index; 
+    });
+
+    for (var w of filtered) {
+        if(keyCount.hasOwnProperty(w)) 
+            keyCount[w]++;
+    };
 }
